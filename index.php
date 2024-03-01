5d39bd7058bdfdff35521a671b741cc10c587bcc
BiancaTinsay <160307603+BiancaTinsay@users.noreply.github.com>
2024-03-01 22:43:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kakegurui in between</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <style>
+
+.responsive-text {
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: 500;
+    color: red;
+    text-align: center;
+    margin: 2%; /* Set margin as 5% of the viewport width and height */
+    position: relative;
+    transition: transform 0.3s;
+}
+
+.responsive-text:hover {
+    transform: scale(1.1);
+    color: #8B0000;
+    transition: color 0.2s ease-in;
+}
+
+
+
+</style>
+    <div class="image-container">
+        <img src="kakegurui-pic.png" alt="bg-img" class="responsive-img">
+        <img src="inspired.png" alt="top-img" class="top-img">
+    </div>
+
+   
+<h5 class="responsive-text">START GAME</h5>
+<h5 class="responsive-text">MECHANICS</h5>
+<h5 class="responsive-text">EXIT</h5>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
